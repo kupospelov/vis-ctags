319ec3d6869c319ea0438f84c6369daee4a68343
Evan Gates <evan@gnarbox.com>
2018-06-05 10:08:45 -0700

    move nil check into separate get_path() function

--- ctags.lua
+++ ctags.lua
@@ -11,9 +11,6 @@ local function get_path(prefix, path)
 end
 
 local function find_tags(path)
-	if path == nil then
-		path = os.getenv('PWD') .. '/'
-	end
 	for i = #path, 1, -1 do
 		if path:sub(i, i) == '/' then
 			local prefix = path:sub(1, i)
@@ -171,9 +168,15 @@ local function pop_pos()
 	end
 end
 
+local function get_path()
+	if vis.win.file.path == nil then
+		return os.getenv('PWD') .. '/'
+	end
+	return vis.win.file.path
+end
+
 local function tag_cmd(tag)
-	local path = vis.win.file.path
-	local match = get_match(tag, path)
+	local match = get_match(tag, get_path())
 	if match == nil then
 		vis:info(string.format('Tag not found: %s', tag))
 	else
@@ -182,8 +185,7 @@ local function tag_cmd(tag)
 end
 
 local function tselect_cmd(tag)
-	local path = vis.win.file.path;
-	local matches = get_matches(tag, path)
+	local matches = get_matches(tag, get_path())
 	if matches == nil then
 		vis:info(string.format('Tag not found: %s', tag))
 	else
